2be9bb5a8c2a1fa1355debb2cd6fd97bf57d066a
Marc Martí Ochaita <marcxeresa@gmail.com>
2026-06-04 11:28:43 +0200

    Update g_comunicaciones.ino

--- Codigos Arduino MQTT/g_comunicaciones.ino
+++ Codigos Arduino MQTT/g_comunicaciones.ino
@@ -1,14 +1,14 @@
+// se suscribe a los topics declarados en el config.h
 void suscribirseATopics() {
   mqtt_subscribe(CAJA_TOPIC);
   mqtt_subscribe(PAQ_TOPIC);
   mqtt_subscribe(STOP_TOPIC);
 }
 
-// Necesaria para que compile la librería MQTT
 void alRecibirMensajePorTopic(char* topic, String incomingMessage) {
-  // No usamos recepción MQTT en este proyecto
 }
 
+//funcion para publicar mensajes en topics
 void enviarMensajePorTopic(const char* topic, String outgoingMessage) {
   mqtt_publish(topic, outgoingMessage.c_str());
 }
